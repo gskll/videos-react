390ff9f024e0681a94b32e26ef8c7cb89c7ce71d
Andrew <gskll@protonmail.com>
2020-11-25 18:16:39 +0100

    feat: display video details as list

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,21 @@
+import "./VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+  const { title, thumbnails } = video.snippet;
+
+  return (
+    <div className="item video-item">
+      <img
+        className="ui image"
+        src={thumbnails.medium.url}
+        alt={`${title} thumbnail`}
+      />
+      <div className="content">
+        <div className="header">{title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
